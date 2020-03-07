bbd17db447583a61435f729a007e28f2b15f6cec
tomkowic <tomkowic@vnode-01.dydaktyka.icsr.agh.edu.pl>
2020-03-07 11:34:24 +0100

    Simple communication working in main.

--- homework.c
+++ homework.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include <mpi.h>
 
-void do_MPI_Send(int frm_proc, int to_proc, int* buf)
+void do_MPI_Send(int from_proc, int to_proc, int* buf)
 {
-    buf = from_proc * 111;
-    MPI_Send(&buf, 1, MPI_INT, to_proc, 123, MPI_COMM_WORLD);
-    printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, buff, to_proc);
+    *buf = from_proc * 111;
+    MPI_Send(buf, 1, MPI_INT, to_proc, 123, MPI_COMM_WORLD);
+    printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, *buf, to_proc);
 }
 
-void do_MPI_Recv(int frm_proc, int to_proc, int* buf, MPI_Status status)
+void do_MPI_Recv(int from_proc, int to_proc, int* buf, MPI_Status status)
 {
-    MPI_Recv(&buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
-    if (buffer == -1)
-        printf("Error: buffer[%d] = %d but is expected to be %d\n", i, buffer[i], i);
-    else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, buff, from_proc);
+    int expected_buf = from_proc * 111;
+    MPI_Recv(buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
+    if (*buf != expected_buf)
+        printf("Error: buffer = %d but is expected to be %d\n", *buf, expected_buf);
+    else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, *buf, from_proc);
     fflush(stdout);
 }
 
@@ -43,18 +44,19 @@ int main (int argc, char * argv[])
         int to_proc = rank + 1;
         buf = 0;
         MPI_Send(&buf, 1, MPI_INT, to_proc, 123, MPI_COMM_WORLD);
-        printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, buff, to_proc);
+        printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, buf, to_proc);
     }
     
     if (rank == 1)
     {
-        int from_proc = rank;
-        int to_proc = rank - 1;
-        buf = -1;
+        int from_proc = rank - 1;
+        int to_proc = rank;
+        int expected_buf = from_proc * 111;
+	buf = -1;
         MPI_Recv(&buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
-        if (buffer == -1)
-            printf("Error: buffer[%d] = %d but is expected to be %d\n", i, buffer[i], i);
-        else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, buff, from_proc);
+        if (buf != expected_buf)
+            printf("Error: buffer = %d but is expected to be %d\n", buf, expected_buf);
+        else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, buf, from_proc);
         fflush(stdout);
     }
     MPI_Finalize();
