5c97c181ede31144490493ed76eeef28eb482b52
tomkowic <tomkowic@vnode-01.dydaktyka.icsr.agh.edu.pl>
2020-03-07 11:53:23 +0100

    Basic communication in functions works.

--- homework.c
+++ homework.c
@@ -9,10 +9,10 @@ void do_MPI_Send(int from_proc, int to_proc, int* buf)
     printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, *buf, to_proc);
 }
 
-void do_MPI_Recv(int from_proc, int to_proc, int* buf, MPI_Status status)
+void do_MPI_Recv(int from_proc, int to_proc, int* buf, MPI_Status* status)
 {
     int expected_buf = from_proc * 111;
-    MPI_Recv(buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
+    MPI_Recv(buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, status);
     if (*buf != expected_buf)
         printf("Error: buffer = %d but is expected to be %d\n", *buf, expected_buf);
     else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, *buf, from_proc);
@@ -38,26 +38,12 @@ int main (int argc, char * argv[])
         return 0;
     }
     
-    if (rank == 0)
-    {
-        int from_proc = rank;
-        int to_proc = rank + 1;
-        buf = 0;
-        MPI_Send(&buf, 1, MPI_INT, to_proc, 123, MPI_COMM_WORLD);
-        printf("Process %d MPI_Send buffer %d to %d.\n", from_proc, buf, to_proc);
-    }
+    if (rank == 0) do_MPI_Send(rank, rank+1, &buf);
     
     if (rank == 1)
     {
-        int from_proc = rank - 1;
-        int to_proc = rank;
-        int expected_buf = from_proc * 111;
 	buf = -1;
-        MPI_Recv(&buf, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
-        if (buf != expected_buf)
-            printf("Error: buffer = %d but is expected to be %d\n", buf, expected_buf);
-        else printf("Process %d MPI_Recv buffer %d from process %d.\n", to_proc, buf, from_proc);
-        fflush(stdout);
+	do_MPI_Recv(rank-1, rank, &buf, &status);
     }
     MPI_Finalize();
     return 0;
