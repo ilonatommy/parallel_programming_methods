ced2d2778bf8fb29e77443260ea3cca3321e5002
tomkowic <tomkowic@vnode-01.dydaktyka.icsr.agh.edu.pl>
2020-03-11 08:21:57 +0100

    Lab_2

--- lab_2/z2.c
+++ lab_2/z2.c
@@ -13,7 +13,7 @@ void get_device_info(int* rank, int*size)
 int main(int argc, char** argv) {
     int rank, size, i;
     const int   ELEM_PER_PROCESS = 1250000; 
-    double start_t, elapsed_t;
+    double start_t_1, start_t_2, elapsed_t_1, elapsed_t_2;
     double glob_elapsed;
      
     MPI_Init(&argc, &argv);
@@ -33,22 +33,24 @@ int main(int argc, char** argv) {
      
     //make all processes wait for each other
     MPI_Barrier(MPI_COMM_WORLD);
-    start_t = MPI_Wtime();
+    start_t_1 = MPI_Wtime();
 
     //send the table of ELEM_PER_PROCESS elements to each of the processes
     MPI_Scatter(collective_array, ELEM_PER_PROCESS, MPI_INT, sub_array, ELEM_PER_PROCESS, MPI_INT, 0, MPI_COMM_WORLD);
-    
+    elapsed_t_1 = MPI_Wtime() - start_t_1;
+
     //do the computations for each rank in parallel
     //for(i=0; i<ELEM_PER_PROCESS; i++) sub_array[i] *= rank;
     
+    start_t_2 = MPI_Wtime();
     MPI_Gather(sub_array, ELEM_PER_PROCESS, MPI_INT, collective_array, ELEM_PER_PROCESS, MPI_INT, 0, MPI_COMM_WORLD);
-    elapsed_t = MPI_Wtime() - start_t;
+    elapsed_t_2 = MPI_Wtime() - start_t_2;
 
     //now wait for all ranks and check timing:
     MPI_Barrier(MPI_COMM_WORLD);
-    glob_elapsed = MPI_Wtime() - start_t;
+    glob_elapsed = MPI_Wtime() - start_t_1;
 
-    printf("%g in rank: %d.\n", elapsed_t, rank);
+    printf("Scatter: %g, Gather: %g in rank: %d.\n", elapsed_t_1, elapsed_t_2, rank);
     fflush(stdout);
     
     if(rank == 0)
--- lab_2/z3.c
+++ lab_2/z3.c
@@ -4,41 +4,27 @@
 #include <assert.h>
 #include <string.h>
 
-//https://stackoverflow.com/questions/52797233/problems-implementing-mpi-scatter-with-mpi-send-and-mpi-recv?rq=1
-
-void Scatter(void* send_data, int send_count, MPI_Datatype send_datatype, 
-                  void* recv_data, int recv_count, MPI_Datatype recv_datatype, 
-                  int root, MPI_Comm communicator) {
-
-    int np, rank;
-    int i;
-    int size;
-
-    MPI_Datatype type;
-    type = send_datatype;
-    MPI_Type_size(type, &size);
-
-    MPI_Status status;
-    MPI_Comm_size(communicator, &np);
-    MPI_Comm_rank(communicator, &rank);
-
-
-
-    if (rank == 0) {
-        for (i = 0; i < np; i++) {
-            MPI_Send(send_data + ((i * send_count) * size), send_count, send_datatype, i, 0, communicator);
-
-        }
-    }
-    MPI_Recv(recv_data, recv_count, recv_datatype, root, 0, communicator, &status);
-
-}
-
 void get_device_info(int* rank, int*size)
 {
     MPI_Comm_rank (MPI_COMM_WORLD, rank);  /* get current process id */
     MPI_Comm_size (MPI_COMM_WORLD, size);  /* get number of processes */
-    //printf("Current process id %d, number of processes %d.\n", *rank, *size);
+}
+
+void Scatter(int* send_data, int* recv_data, int count) {
+    int size, rank, i;
+    int type_size;
+
+    MPI_Datatype type = MPI_INT;
+    MPI_Type_size(type, &type_size);
+
+    MPI_Status status;
+    get_device_info(&rank, &size);
+
+    if (rank == 0){
+        for (i = 1; i < size; i++){
+            MPI_Send(send_data + ((i * count) * type_size), count, MPI_INT, i, 123, MPI_COMM_WORLD);
+        }
+    } else MPI_Recv(recv_data, count, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 }
 
 int main(int argc, char** argv) {
@@ -59,20 +45,15 @@ int main(int argc, char** argv) {
     }
     
     //allocate small arrays for each process
-    int sub_array[ELEM_PER_PROCESS];
+    int* sub_array = (int*)malloc(sizeof(int)*ELEM_PER_PROCESS);
     assert(sub_array != NULL);    
      
     //make all processes wait for each other
     MPI_Barrier(MPI_COMM_WORLD);
     start_t = MPI_Wtime();
 
-    //send the table of ELEM_PER_PROCESS elements to each of the processes
-    //MPI_Scatter(collective_array, ELEM_PER_PROCESS, MPI_INT, sub_array, ELEM_PER_PROCESS, MPI_INT, 0, MPI_COMM_WORLD);
     //custom scatter:
-    Scatter(collective_array, ELEM_PER_PROCESS, MPI_INT, sub_array, ELEM_PER_PROCESS, MPI_INT, 0, MPI_COMM_WORLD);    
-
-    //do the computations for each rank in parallel
-    for(i=0; i<ELEM_PER_PROCESS; i++) sub_array[i] *= rank;
+    Scatter(collective_array, sub_array, ELEM_PER_PROCESS);     
     
     MPI_Gather(sub_array, ELEM_PER_PROCESS, MPI_INT, collective_array, ELEM_PER_PROCESS, MPI_INT, 0, MPI_COMM_WORLD);
     elapsed_t = MPI_Wtime() - start_t;
@@ -86,11 +67,12 @@ int main(int argc, char** argv) {
     
     if(rank == 0)
     {
-	for(i=0; i<len; i++) printf("%d, ", collective_array[i]);
-        //printf("\n---------------------------\n%g globally.\n---------------------------\n", glob_elapsed);
+        printf("\n---------------------------\n%g globally.\n---------------------------\n", glob_elapsed);
+        fflush(stdout);
         free(collective_array);
+       
     }  
-   
+   free(sub_array);
     MPI_Finalize();
     return 0;
 } 
