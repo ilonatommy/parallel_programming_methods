59872661eae0f167ced011627d9d791f4521d92f
tomkowic <tomkowic@vnode-01.dydaktyka.icsr.agh.edu.pl>
2020-03-15 20:19:54 +0100

    Time meassurement was added.

--- lab_3/pi_par.c
+++ lab_3/pi_par.c
@@ -10,10 +10,15 @@ int main(int argc, char* argv[]) {
  //pozwala na trzymanie sumarycznie aż do 18,446,744,073,709,551,61 punktów, które znajdziemy w środku koła
  //innym sposobem byloby liczenie pi osobno w każdym procesie i usrednianie wyniku
  long int sendbuf, recvbuf;
+ double start_t, end_t;
+ 
  MPI_Init(&argc, &argv);
  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
  MPI_Comm_size(MPI_COMM_WORLD, &size);
  
+ MPI_Barrier(MPI_COMM_WORLD);
+ start_t = MPI_Wtime();
+
  srand( SEED );
  int i, count, n;
  double x,y,z,pi;
@@ -32,20 +37,19 @@ int main(int argc, char* argv[]) {
  sendbuf = count;
  MPI_Reduce(&sendbuf, &recvbuf, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
+ MPI_Barrier(MPI_COMM_WORLD);
+ end_t = MPI_Wtime() - start_t;
+
  //sprawdzanie liczby PI tylko w jednym procesie
  if(rank == 0)
  {
-    //zbierz wszystkie punkty wewnątrz koła od procesów (sumując)
-    //MPI_Reduce(&sendbuf, &recvbuf, n, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     av_points = (recvbuf) / size;
     pi = (double) av_points / n * 4; 
     printf("Approximate PI = %g.\n", pi);
+    printf("Execution time: %g\n.", end_t);
  }
  
-//free( sendbuf );
-//free( recvbuf );
- 
-MPI_Finalize();
-return(0);
+ MPI_Finalize();
+ return(0);
 }
  
