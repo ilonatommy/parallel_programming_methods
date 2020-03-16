c20b489b2c5af9f1609032bf2545823ffdd6fd05
tomkowic <tomkowic@vnode-01.dydaktyka.icsr.agh.edu.pl>
2020-03-16 09:05:37 +0100

    Sequential version ready to sumbit

--- lab_3/pi_seq.c
+++ lab_3/pi_seq.c
@@ -5,30 +5,52 @@
 #include <time.h>
 #define SEED time(NULL)
 
-int main() {
+int main(int argc, char* argv[]) {
 
  srand( SEED );
- int i, count, n;
+ long int n, i, count;
  double x,y,z,pi;
-
- n = 1000000000;
-
- count = 0;
-
- for(i = 0; i < n; ++i) {
-
-     x = (double)rand() / RAND_MAX;
-
-     y = (double)rand() / RAND_MAX;
-
-     z = x * x + y * y;
-
-     if( z <= 1 ) count++;
- }
-
- pi = (double) count / n * 4;
+ char* eptr;
  
- printf("Approximate PI = %g\n", pi);
+ printf("You passed: %s as a number of points.\n", argv[1]);
+ //ilosc punktow podajemy jako argument
+ n =  strtol(argv[1], &eptr, 10);
 
+ //petla usredniajaca: 100-krotna
+ int iterations, j;
+ double* all_pi;
+ double sd, summed_pi;
+ iterations = 100;
+ summed_pi = 0;
+ sd = 0;
+ all_pi = (double*)malloc(iterations*sizeof(double));
+
+ for(j=0; j<iterations; j++){
+  count = 0;
+
+  for(i = 0; i < n; ++i) {
+      //losowanie niezależne:
+      x = (double)rand() / RAND_MAX;
+      y = (double)rand() / RAND_MAX;
+     
+      //liczenie długości wektora:
+      z = x * x + y * y;
+     
+      //sprawdzenie czy jest w kole:
+      if( z <= 1 ) count++;
+  }
+  all_pi[j] = (double) count / n * 4;
+  summed_pi += all_pi[j];
+ }
+ 
+ //średnia:  
+ pi = summed_pi / iterations;
+ printf("PI = %g\n", pi);
+ 
+ //odchylenie:
+ for(j=0; j<iterations; j++)
+  sd += pow(all_pi[j] - pi, 2); 
+ sd = sqrt(sd/iterations);
+ printf("SD = %g\n", sd);
  return(0);
 }
