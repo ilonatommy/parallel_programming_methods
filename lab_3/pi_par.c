3132f70fb7e7df074ea3538f4cf06bb98916e3ff
ilonatommy <ili.ili@interia.pl>
2020-03-16 09:31:44 +0100

    SD and av_loop were added

--- lab_3/pi_par.c
+++ lab_3/pi_par.c
@@ -6,10 +6,10 @@
 #define SEED time(NULL)
 
 int main(int argc, char* argv[]) {
- int rank, size, av_points;
+ int rank, size;
  //pozwala na trzymanie sumarycznie aż do 18,446,744,073,709,551,61 punktów, które znajdziemy w środku koła
  //innym sposobem byloby liczenie pi osobno w każdym procesie i usrednianie wyniku
- long int sendbuf, recvbuf;
+ double sendbuf_1, recvbuf_1, sendbuf_2, recvbuf_2;
  double start_t, end_t;
  
  MPI_Init(&argc, &argv);
@@ -23,30 +23,59 @@ int main(int argc, char* argv[]) {
  int i, count, n;
  double x,y,z,pi;
  
- n = 2000000; //liczba losowań w każdym procesie
- count = 0; //liczba punktów znalezionych w kole
+ n = 100; //liczba losowań w każdym procesie
  
- //losowanie w każdym procesie:
- for(i = 0; i < n; ++i) {
-     x = (double)rand() / RAND_MAX;
-     y = (double)rand() / RAND_MAX;
-     z = x * x + y * y;
+ //petla usredniajaca: 100-krotna
+ int iterations, j;
+ double* all_pi;
+ double sd, summed_pi;
+ iterations = 100;
+ summed_pi = 0;
+ sd = 0;
+ all_pi = (double*)malloc(iterations*sizeof(double));
+ 
+ for(j=0; j<iterations; j++){
+ count = 0; //liczba punktów znalezionych w kole
+  
+  //losowanie n punktów:
+  for(i = 0; i < n; ++i) {
+      x = (double)rand() / RAND_MAX;
+      y = (double)rand() / RAND_MAX;
+      z = x * x + y * y;
 
-     if( z <= 1 ) count++;
+      if( z <= 1 ) count++;
+  }
+  all_pi[j] = (double) count / n * 4;
+  summed_pi += all_pi[j];  
  }
- sendbuf = count;
- MPI_Reduce(&sendbuf, &recvbuf, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
+ 
+ //średnie pi obliczone przez jeden proces:  
+ pi = summed_pi / iterations;
+ sendbuf_1 = pi;
+ 
+ //zsumuj wszystkie średnie pi z procesów i zapisz do recvbuf
+ MPI_Reduce(&sendbuf_1, &recvbuf_1, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+ 
+ //odchylenie:
+ for(j=0; j<iterations; j++)
+  sd += pow(all_pi[j] - pi, 2); 
+ sendbuf_2 = sqrt(sd/iterations);
+ 
+ //zsumuj wszystkie odchylenia, bo będziemy je uśredniać w procesie głównym
+ MPI_Reduce(&sendbuf_2, &recvbuf_2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+ 
  MPI_Barrier(MPI_COMM_WORLD);
  end_t = MPI_Wtime() - start_t;
 
- //sprawdzanie liczby PI tylko w jednym procesie
+ //tylko proces główny oblicza ostateczny wynik 
+ //to jest dość szybkie, więc nie musimy brać pod uwagę w czasie pracy programu
  if(rank == 0)
  {
-    av_points = (recvbuf) / size;
-    pi = (double) av_points / n * 4; 
-    printf("Approximate PI = %g.\n", pi);
+    pi = recvbuf_1 / size;
+    sd = recvbuf_2 / size;
+    printf("Approximate PI = %g.\n.", pi);
     printf("Execution time: %g\n.", end_t);
+    printf("SD = %g\n.", sd);
  }
  
  MPI_Finalize();
