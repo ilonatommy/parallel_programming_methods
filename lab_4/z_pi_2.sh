28bec49dacd5a439e90016a665070e79a6eea611
Ilona Tomkowicz <plgilonat@zeus.cyfronet.pl>
2020-03-23 16:53:56 +0100

    Changes on Zeus lab_4

---
+++ lab_4/z_pi_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --ntasks 12
+#SBATCH --time=01:00:00
+#SBATCH --partition=plgrid
+#SBATCH --account=plgilonat2020a
+module add plgrid/tools/openmpi
+
+mpicc -o pi_par pi_par.c
+for ((i=1; i<=12; i++)); do
+        declare -i n=$(( 100000000 * i))
+	mpiexec -np $i ./pi_par $n 
+done
