f6e6a573e3bf98ba71d20eaa59b623eac91c3673
ilonatommy <ili.ili@intera.pl>
2020-03-28 10:47:37 +0100

    big size partial results

--- lab_4/z_pi_final.sh
+++ lab_4/z_pi_final.sh
@@ -7,12 +7,12 @@
 #SBATCH --partition=plgrid
 #SBATCH --account=plgilonat2020a
 module add plgrid/tools/openmpi
-I=(1 2 3)
-SIZE=(10000000 1000000000 40000000000)
+I=(1)
+SIZE=(40000000000)
 mpicc -o pi_par pi_par.c
 for i in ${I[@]}; do
 	for s in ${SIZE[@]}; do
-		for ((i=1; i<=12; i++)); do
+		for ((i=7; i<=12; i++)); do
         		mpiexec -np $i ./pi_par $s
         		declare -i n=$(( s * i))
 			mpiexec -np $i ./pi_par $n
